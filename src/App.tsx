bba8608606df2143cfce323ecc925e68b48ec2dc
Dio Risso <helenamarxr@gmail.com>
2024-11-26 19:26:49 -0300

    created second section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css'
+import './App.css';
+import './mobile.css';
 
 import Header from './components/Header';
 import mariaDolores from './assets/mariadolores.png';
 import aruna from './assets/aruna.png';
 import vicente from './assets/vicente.png';
+import alencar from './assets/alencar.png';
 
 function App() {
 
@@ -23,7 +25,28 @@ function App() {
       </section>
 
       <section className="second">
+        <div className="text-container">
+          <div className="text-content">
+            <h1>Nossa História</h1>
+            <p>
+              <span>A Vicente Empreendimentos</span> nasceu do sonho e da determinação de 
+              <span> Alencar Maurício Vicente</span>, que desde jovem superou desafios para 
+              transformar sua vida.<br/><br/> Natural de Indaial, Santa Catarina, Alencar 
+              mudou-se ainda criança para <span>Itapema</span>, onde trabalhou em diversas 
+              áreas, desde vendedor de picolé até a construção civil.<br/><br/> Determinado 
+              a buscar novas oportunidades, Alencar ingressou em um escritório de 
+              arquitetura, onde aprimorou suas habilidades. <span>Em 2019, ele fundou 
+              A Vicente Empreendimentos.</span>
+            </p>
+          </div>
+        </div>
 
+        <div className="photo-container">
+          <div className="photo-content">
+            <img src={alencar} alt="Alencar Vicente" />
+            <h1>Alencar Maurício Vicente</h1>
+          </div>
+        </div>
       </section>
     </>
   )
