c21936dd09626d59daf5cf9a02716e4a7ada9df2
Dio Risso <helenamarxr@gmail.com>
2024-12-11 19:29:51 -0300

    refactored first section

--- src/components/Inicio/index.tsx
+++ src/components/Inicio/index.tsx
@@ -1,15 +1,22 @@
 import "./styles.css";
+import { motion } from 'motion/react';
 
 export default function Inicio () {
     return (
         <div className="first-container">
             <section id="inicio" className="first">
-                <div className="content">
+                <motion.div 
+                    transition={{ duration: 0.8 }} 
+                    initial={{opacity:0, y:-55}} 
+                    whileInView={{opacity:1, y:0}} 
+                    viewport={{once:true}} 
+                    className="content"
+                >
                     <div className="headline">
-                        <h1 className="noselect"><span>Priorizamos a excelência em cada um de nossos empreendimentos.</span></h1>
+                        <h1>Priorizamos a <span>excelência</span> em cada um de nossos empreendimentos.</h1>
+                        <a href="#contato"><button><span>Entrar em contato</span></button></a>
                     </div>
-                    <a href="#contato"><button>Entrar em contato</button></a>
-                </div>
+                </motion.div>
             </section>
             <div className="first-bg"/>
         </div>
